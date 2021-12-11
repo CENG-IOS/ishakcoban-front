6f238a98567bc461952585f192e9e8e2da0e0a36
Khan <oguzhanercelik@eskisehir.edu.tr>
2021-12-11 17:22:58 +0300

    navbar is done

--- src/App.js
+++ src/App.js
@@ -4,14 +4,17 @@ import { Logo } from "./components/homePagelines/Logo";
 import { ViewButton } from "./components/buttons/homePageButtons/ViewButton";
 import { ForPractise } from "./pages/ForPractise";
 import { Footer } from "./components/Footer";
+import Header from "./components/Header";
+
 function App() {
-  document.body.classList.add('body-background');
-  return (
-    <div>
-    <Home></Home>
-<Footer></Footer>
-    </div>
-  );
+    document.body.classList.add('body-background');
+    return (
+        <div>
+            <Header />
+            <Home></Home>
+            <Footer></Footer>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,27 @@
+import React from "react";
+import Navbar from 'react-bootstrap/Navbar'
+import Container from 'react-bootstrap/Container'
+import Nav from 'react-bootstrap/Nav'
+
+const Header = props => {
+
+    return (
+        <Navbar bg="light" expand="lg">
+            <Container fluid className="px-5">
+                <Navbar.Brand className="main-logo user-select-none col-lg-6">ishakcoban</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
+                    <Nav>
+                        <Nav.Link href="/" className="d-flex justify-content-center">ANA SAYFA</Nav.Link>
+                        <Nav.Link href="#link" className="d-flex justify-content-center">ÇİZİMLER</Nav.Link>
+                        <Nav.Link href="#link" className="d-flex justify-content-center">FOTOĞRAFLAR</Nav.Link>
+                        <Nav.Link href="#link" className="d-flex justify-content-center">HAKKINDA</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar >
+
+    )
+}
+
+export default Header; 
