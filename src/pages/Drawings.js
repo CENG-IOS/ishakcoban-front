f2d3b02078f14acc3047b4699da345e0f937c7aa
ishakcoban <ishakcoban99@outlook.com>
2022-02-08 19:44:18 +0300

    images cropped successfully

--- src/pages/Drawings.js
+++ src/pages/Drawings.js
@@ -1,23 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { FilterSection } from '../components/FilterSection'
-import viviChan from "../images/cnsv21.png"
+import svdrgn from "../images/svd2.jpg"
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { CardActionArea } from '@mui/material';
 export const Drawings = () => {
+    const [size, findSize] = useState(false)
+    const clicked = () => {
+        const img = new Image();
+        img.onload = function () {
+            if (this.width > this.height) {
+                findSize(!size)
+            }
+
+        }
+        img.src = svdrgn;
+    }
     return (
         <div className='mt-5 mb-5'>
             <FilterSection filterType='drawing'></FilterSection>
 
 
+            <div class="img-wrapper">
+                <img className='each-img' src={svdrgn}></img>
+                <img className='each-img' src={svdrgn}></img>
+                <img className='each-img' src={svdrgn}></img>
+                <img className='each-img' src={svdrgn}></img>
+                <img className='each-img' src={svdrgn}></img>
+                <img className='each-img' src={svdrgn}></img>
+                <img className='each-img' src={svdrgn}></img>
+                <img className='each-img' src={svdrgn}></img>
+                <img className='each-img' src={svdrgn}></img>
+                <img className='each-img' src={svdrgn}></img>
+                <img className='each-img' src={svdrgn}></img>
+                <img className='each-img' src={svdrgn}></img>
 
-            <div className='d-flex justify-content-center bg-info'>
-                <div className='grid-system'>asdsasadads sa sacd asd sas ss sadas sads sds sd</div>
-               
             </div>
-
-
-
-
-
-
 
 
 
